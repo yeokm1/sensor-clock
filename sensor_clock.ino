9f8bd97c0b9511661deb31112791f567ffd919a9
Yeo Kheng Meng <yeokm1@gmail.com>
2014-04-05 15:35:10 +0800

    removed initial value

--- sensor_clock.ino
+++ sensor_clock.ino
@@ -21,9 +21,6 @@
 
 #define LCD_DEGREES_SYMBOL (char) 223
 
-#define INITIAL_DHT22_HUM 50;  //Celsius
-#define INITIAL_DHT22_TEMP 30; //Percent
-
 #define REFRESH_INTERVAL 5 //Minimum 5 seconds for DHT22 to refresh. Do not exceed 59
 #define SWAP_LINE2_Interval 15 //Must be multiple of refresh interval. Do not exceed 59
 
